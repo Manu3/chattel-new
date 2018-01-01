81131a339985b210af1a14711e802bf5bf095e6e
Manu3 <manu.kapoor91@gmail.com>
2018-01-02 01:08:01 +0530

    added server files

---
+++ server/models/cars.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var cars = mongoose.model('cars', {
+
+  name:{
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true
+  },
+  seater:{
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true
+  },
+  rateWeekdays:{
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true
+  },
+  rateWeekend:{
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true
+  }
+});
+
+module.exports = {cars};
